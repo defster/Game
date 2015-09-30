abc755515f3f7573a734e18b0fd1e7bc207850b1
Øyvind Husabø <ohusabo@gmail.com>
2015-09-30 13:58:52 +0200

    First push.

---
+++ SlimDX1/Program.cs
@@ -0,0 +1,13 @@
+﻿using SlimDX.Windows;
+
+namespace SlimDX1
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var form = new RenderForm("Tutorial 1: Basic Window");
+            MessagePump.Run(form, () => { });
+        }
+    }
+}
